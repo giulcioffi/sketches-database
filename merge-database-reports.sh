f1fd4d8060da07997984d4503ed8767a5f08e613
giulcioffi <g.cioffi@arduino.cc>
2020-10-08 12:59:21 +0200

    Fix error that removed the sizes key only for the first array element

--- merge-database-reports.sh
+++ merge-database-reports.sh
@@ -8,7 +8,7 @@ echo "Merging the board-related json files from the database into a single repor
 
 jq -s '.[0].boards=([.[].boards]|flatten)|.[0]' database-reports/*.json >> database-reports/full-database-report.json
 
-jq 'del(.boards[0].sketches[].sizes)' database-reports/full-database-report.json >> database-reports/modified-full-database-report.json
+jq 'del(.boards[].sketches[].sizes)' database-reports/full-database-report.json >> database-reports/modified-full-database-report.json
 
 mv database-reports/modified-full-database-report.json database-reports/full-database-report.json
 
